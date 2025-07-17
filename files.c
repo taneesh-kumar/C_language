a36054233d64bcb8d5c00d900095373afa557f40
taneesh-kumar <heyitstaneesh@gmail.com>
2025-07-17 14:55:22 +0530

    update

--- files.c
+++ files.c
@@ -2,6 +2,16 @@
 
 int main(){
     FILE *ptr;
-    ptr = fopen();
+    ptr = fopen("files_testing.exe", "a");
+
+    if (ptr == NULL){
+        printf("Sorry, the file doesn't Exist\n");
+    } else{
+        int num = 143;
+        fprintf(ptr, "%d", num);
+        printf("The value of num is : %d\n", num);
+        fclose(ptr);
+    }
+
     return 0;
 }
